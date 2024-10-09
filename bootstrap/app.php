f3e58a44eadaa4cc1992e9cf169d65c5a7280139
Carlos Souza <carlos@i-databi.com.br>
2024-10-09 16:30:40 -0300

    feat: Adicionado commando para rodar via schedule

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,6 +10,9 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
+    ->withSchedule(function ($schedule) {
+        $schedule->command('app:close-project')->everyMinute();
+    })
     ->withMiddleware(function (Middleware $middleware) {
         //
     })
